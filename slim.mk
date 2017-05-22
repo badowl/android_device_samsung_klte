7a2ed8685791a519cffc4fa27e31cf5f7af6a62a
badowl <badowl@gmx.at>
2017-05-22 22:22:17 +0200

    Update slim.mk

--- slim.mk
+++ slim.mk
@@ -21,17 +21,31 @@ PRODUCT_RELEASE_NAME := klte
 # TARGET_SCREEN_HEIGHT := 1920
 # TARGET_SCREEN_WIDTH := 1080
 # Inherit some common Slim stuff.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from klte device
+$(call inherit-product, device/samsung/klte/full_klte.mk)
+
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 # Inherit device configuration
-$(call inherit-product, device/samsung/klte/aosp_klte.mk)
+
 PRODUCT_DEVICE := klte
 PRODUCT_NAME := slim_klte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := G900x
-PRODUCT_MANUFACTURER := Samsung
-PRODUCT_RESTRICT_VENDOR_FILES := false
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BRAND := samsung
+TARGET_VENDOR := samsung
+TARGET_VENDOR_PRODUCT_NAME := klte
+TARGET_VENDOR_DEVICE_NAME := G900x
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=G900x PRODUCT_NAME=klte
+
+## Use the latest approved GMS identifiers unless running a signed build
+ifneq ($(SIGN_BUILD),true)
 PRODUCT_BUILD_PROP_OVERRIDES += \
-PRODUCT_NAME=klte\
     BUILD_FINGERPRINT=samsung/klte/G900x:6.0.1/MHC19Q/ZNH2KAS1KN:user/release-keys \
     PRIVATE_BUILD_DESC="klte-user 6.0.1 MHC19Q ZNH2KAS1KN release-keys"
-
+endif
